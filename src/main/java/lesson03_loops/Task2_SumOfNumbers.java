902e20ee7d5c37e18359bf99e254cad67adde6be
mehronbl15 <mehrobmirzoyorov8@gmail.com>
2026-07-12 11:43:56 +0300

    Task4_FizzBuzz (классическая задача с собеседований!): вывести числа от 1 до 20, но:
    если число делится на 3 — вывести "Fizz"
    если делится на 5 — вывести "Buzz"
    если делится и на 3, и на 5 — вывести "FizzBuzz"
    иначе — само число

--- src/main/java/lesson03_loops/Task2_SumOfNumbers.java
+++ src/main/java/lesson03_loops/Task2_SumOfNumbers.java
@@ -2,6 +2,12 @@ package lesson03_loops;
 
 public class Task2_SumOfNumbers {
     public static void main(String[] args){
-
+        int sum =0;
+        int number=1;
+        while (number<=100){
+            sum=sum+number;
+            number++;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lesson03_loops/Task4_FizzBuzz.java
+++ src/main/java/lesson03_loops/Task4_FizzBuzz.java
@@ -2,8 +2,6 @@ package lesson03_loops;
 
 public class Task4_FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
+
     }
 }
